a2327dd49dc13fde7d5d44b4a50a95fd3cd01785
Ryan Evans <revans17@masonlive.gmu.edu>
2018-10-04 21:38:51 -0400

    make it better

--- Gemfile
+++ Gemfile
@@ -37,7 +37,6 @@ group :test do
 end
 
 group :production do
-  gem 'rails_12factor'
   gem 'pg',  '0.20.0'
   gem 'fog', '1.42'
 end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAICWH4VRNSR7WXWBA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['NULL'],
+      :aws_secret_access_key => ENV['NULL']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
