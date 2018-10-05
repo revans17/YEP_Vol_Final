84275b1a3c2fd0b9e92140f3e897a7a743d377e1
Ryan Evans <revans17@masonlive.gmu.edu>
2018-10-04 21:17:49 -0400

    make it better

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIGSLPM6VKVYARTWA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['AKIAICWH4VRNSR7WXWBA'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
- 
