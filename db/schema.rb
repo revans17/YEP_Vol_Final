63db4cb20bbdfda17563e7d5bb263bac0ee5c586
Ryan Evans <revans17@masonlive.gmu.edu>
2018-10-25 18:48:33 -0400

    Updated DB with points

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609220802) do
+ActiveRecord::Schema.define(version: 20181025221917) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -40,10 +40,11 @@ ActiveRecord::Schema.define(version: 20160609220802) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated", default: true
+    t.boolean "activated", default: false
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "points"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
