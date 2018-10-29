3061ba55448382b629678c07c275266f153fa2e5
Ryan Evans <revans17@masonlive.gmu.edu>
2018-10-29 19:26:39 -0400

    Updated Images

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user,   only: :destroy
+   before_action :correct_user, only:  :destroy
+   # before_action :admin_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -28,4 +29,8 @@ class MicropostsController < ApplicationController
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
+
+    # def admin_user
+    #   redirect_to(root_url) unless current_user.admin?
+    # end
 end
